a66764421f51de288ee447b6e3ff5d1602211196
Piera di fusco <94469162+me-pdf@users.noreply.github.com>
2025-05-02 14:02:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.page').forEach(page => {
-  page.addEventListener('click', () => {
-    page.style.transform = page.style.transform === 'rotateY(180deg)' ? '' : 'rotateY(180deg)';
-  });
-});
